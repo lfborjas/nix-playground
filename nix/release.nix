8e030ef4dfba3176f7457e0ea15c3d7709664322
Luis Borjas Reyes <luisfborjas@gmail.com>
2021-03-14 18:42:48 -0400

    Getting a very basic haskell build via nix
    Using:
    
        nix-build -A astral-arcanum-app ./nix/monorepo.nix
    
    Also, had to copy the haskell sources from HLS, since
    nixpkgs upstream doesn't seem to have a usable ghc 8.10.2.
    
    Apart from that, following this:
    
    https://github.com/fghibellini/nix-haskell-monorepo/tree/master/monorepo-nix-expressions

--- nix/release.nix
+++ nix/release.nix
@@ -1,4 +1,4 @@
-{ sources ? import ./sources.nix }:
+{ sources ? import ./sources.nix, compiler ? "ghc8102" }:
 with
   { overlay = _: pkgs: 
     {
@@ -7,5 +7,17 @@ with
   };
 import sources.nixpkgs
   { 
-    overlays = [overlay] ; config = {}; 
+    overlays = [ overlay ] ; config = {
+      allowUnfree = true;
+      packageOverrides = pkgs: rec {
+        haskellPackages = pkgs.haskell.packages."${compiler}".override {
+          overrides = self: super: builtins.mapAttrs (name: path: super.callCabal2nix name path {}) (import ./packages.nix);
+        };
+          # haskell = pkgs.haskell // {
+          #   "${compiler}" = pkgs.haskell.packages."${compiler}".override {
+          #     overrides = self: super: builtins.mapAttrs (name: path: super.callCabal2nix name path {}) (import ./packages.nix);
+          #   };
+          # };
+      };
+    }; 
   }
